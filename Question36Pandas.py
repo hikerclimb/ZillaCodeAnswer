9b950bb9c5a35c67ee8e14bbf18aad624e480e68
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-07-23 17:37:39 -0500

    made it so the answer prints from a function

--- Question36Pandas.py
+++ Question36Pandas.py
@@ -18,14 +18,15 @@ funded_startups_df = pd.DataFrame({
     'funding': [2.0, 1.0, 2.5, 2.0, 1.8, 1.7, 3.0, 2.0]
 })
 
-# 2. Merge them together on 'vc_id'
-df_combined = pd.merge(funded_startups_df, venture_capitalist_df, on='vc_id', how='inner')
-average = df_combined.groupby(['vc_id'])['funding'].mean().reset_index(name = 'avg_funding')
-funding_limit = df_combined.groupby(['vc_id'])['funding_limit'].mean().reset_index(name = 'funding_limit')
-if((average['avg_funding'] > funding_limit['funding_limit']).any()):
-    greaterthan = average['avg_funding'] > funding_limit['funding_limit']
-    almostout = pd.merge(average[greaterthan], funding_limit[greaterthan], on="vc_id", how="inner")
-    out = pd.merge(almostout, venture_capitalist_df, on = "vc_id", how= "inner")
-    out.rename(columns={"funding_limit_x": "funding_limit"}, inplace=True)
-    out.drop(columns='funding_limit_y', inplace = True)
-    return out
+def etl(venture_capitalist_df, funded_startups_df):
+    df_combined = pd.merge(funded_startups_df, venture_capitalist_df, on='vc_id', how='inner')
+    average = df_combined.groupby(['vc_id'])['funding'].mean().reset_index(name = 'avg_funding')
+    funding_limit = df_combined.groupby(['vc_id'])['funding_limit'].mean().reset_index(name = 'funding_limit')
+    if ((average['avg_funding'] > funding_limit['funding_limit']).any()):
+        greaterthan = average['avg_funding'] > funding_limit['funding_limit']
+        almostout = pd.merge(average[greaterthan], funding_limit[greaterthan], on="vc_id", how="inner")
+        out = pd.merge(almostout, venture_capitalist_df, on = "vc_id", how= "inner")
+        out.rename(columns={"funding_limit_x": "funding_limit"}, inplace=True)
+        out.drop(columns='funding_limit_y', inplace = True)
+        return out
+print(etl(venture_capitalist_df, funded_startups_df))
