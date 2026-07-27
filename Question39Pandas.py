011497bdb0078fce239f5abae526d6285c5bd5a6
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-07-27 16:03:43 -0500

    got one more working :)

--- Question39Pandas.py
+++ Question39Pandas.py
@@ -4,31 +4,28 @@ import datetime
 import json
 import math
 import re
-transactions_data = {
-    'trans_id': [1, 2, 3],
-    'trans_amt': [500.0, 200.0, 300.0],
-    'date': ['2023-07-01', '2023-07-02', '2023-07-03'],
-    'cust_id': [1001, 1002, 1003]
-    }
 
-transactions = pd.DataFrame(transactions_data)
-    # Convert the 'date' column to datetime format
-    #transactions['date'] = pd.to_datetime(transactions['date'])    
-    # 2. Create customers DataFrame
-customers_data = {
-    'cust_id': [1001, 1002, 1003],
-    'first_name': ['John', 'Jane', 'Bob'],
-    'last_name': ['Doe', 'Smith', 'Johnson'],
-        'age': [30, 40, 50]
+data = {
+    "interaction_id": [1, 2, 3, 4, 5],
+    "user1_id": [1001, 1002, 1003, 1004, 1005],
+    "user2_id": [2002, 1002, 2003, 1004, 2005],
+    "interaction_type": ["like", "comment", "share", "like", "comment"],
+    "timestamp": [
+        "2023-01-01 10:00:00",
+        "2023-01-01 11:00:00",
+        "2023-01-02 10:00:00",
+        "2023-01-02 11:00:00",
+        "2023-01-03 10:00:00",
+    ],
 }
-        
-customers = pd.DataFrame(customers_data)
-    
-def etl(transactions, customers):
-    cross_joined = pd.merge(transactions, customers, how='cross')
-    output = cross_joined.drop(columns=["cust_id_x"])
-    out = output.rename(columns={"cust_id_y": "cust_id"})
-    #output = out[['age','cust_id', 'date', 'first_name', 'last_name', 'trans_amt', 'trans_id']]
-    return out
 
-print(etl(transactions, customers))
+input_df = pd.DataFrame(data)
+
+def etl(input_df):
+    userId = input_df.query("user1_id == user2_id")
+    counts_df = userId['user1_id'].value_counts().reset_index()
+    counts_df.rename(columns={'count': 'self_interaction_count'}, inplace=True)
+    return counts_df
+
+
+print(etl(input_df))
